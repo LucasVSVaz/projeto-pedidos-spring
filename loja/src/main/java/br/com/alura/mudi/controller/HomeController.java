f623f63d42131ae770d2c1d32fb5ca5d29be0735
Lucas Vieira <lucas.v.santosvaz@gmail.com>
2021-09-26 17:38:57 -0300

    VINCULANDO PEDIDOS AOS USUARIOS II

--- loja/src/main/java/br/com/alura/mudi/controller/HomeController.java
+++ loja/src/main/java/br/com/alura/mudi/controller/HomeController.java
@@ -5,9 +5,7 @@ import br.com.alura.mudi.model.StatusPedido;
 import br.com.alura.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,26 +17,12 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private PedidoRepository repository;
+
     @GetMapping
     public ModelAndView home(Principal principal) {
-        List<Pedido> pedidos = repository.findAllByUser(principal.getName());
+        List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE);
         ModelAndView mv = new ModelAndView("/home");
         mv.addObject("pedidos", pedidos);
         return mv;
     }
-
-    @GetMapping("/{status}")
-    public ModelAndView porStatus(@PathVariable("status") String status) {
-        List<Pedido> pedidos = repository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-        ModelAndView mv = new ModelAndView("/home");
-        mv.addObject("pedidos", pedidos);
-        return mv;
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String onError() {
-        return "redirect:/home";
-    }
-
-
 }
