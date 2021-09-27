64de58c0e8ba24f252938cc8098abb454241be2e
Lucas Vieira <lucas.v.santosvaz@gmail.com>
2021-09-27 17:20:47 -0300

    PAGINAÇÃO I

--- loja/src/main/java/br/com/alura/mudi/controller/HomeController.java
+++ loja/src/main/java/br/com/alura/mudi/controller/HomeController.java
@@ -4,6 +4,8 @@ import br.com.alura.mudi.model.Pedido;
 import br.com.alura.mudi.model.StatusPedido;
 import br.com.alura.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,12 @@ public class HomeController {
     @Autowired
     private PedidoRepository repository;
 
+    Sort sort = Sort.by("dataDeEntrega").descending();
+    PageRequest pageRequest = PageRequest.of(0, 10, sort);
+
     @GetMapping
     public ModelAndView home(Principal principal) {
-        List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE);
+        List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE, pageRequest);
         ModelAndView mv = new ModelAndView("/home");
         mv.addObject("pedidos", pedidos);
         return mv;
--- loja/src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
+++ loja/src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mudi.repository;
 
 import br.com.alura.mudi.model.Pedido;
 import br.com.alura.mudi.model.StatusPedido;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByStatus(StatusPedido status);
+    List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
     List<Pedido> findAllByUser(@Param("username") String username);
